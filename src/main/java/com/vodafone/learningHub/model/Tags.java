5ba869460033f1c73dcee63801b963f426626d16
Karim Hesham <karim.hesham5861@gmail.com>
2023-08-15 15:27:23 +0200

    build proj success

--- src/main/java/com/vodafone/learningHub/model/Tags.java
+++ src/main/java/com/vodafone/learningHub/model/Tags.java
@@ -1,8 +1,8 @@
 package com.vodafone.learningHub.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
 @Entity
 @Table(name="tags")
--- src/main/java/com/vodafone/learningHub/service/PostServiceImpl.java
+++ src/main/java/com/vodafone/learningHub/service/PostServiceImpl.java
@@ -1,7 +1,19 @@
 package com.vodafone.learningHub.service;
 
+import com.vodafone.learningHub.model.Post;
+import com.vodafone.learningHub.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PostServiceImpl {
+@RequiredArgsConstructor
+public class PostServiceImpl implements PostServiceI{
+
+    private final PostRepository postRepository;
+
+    @Override
+    public Post createPost(Post post) {
+        
+        return null;
+    }
 }
