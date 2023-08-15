23bccc5de37612aa85007c3c245d3412acefc30f
Karim Hesham <karim.hesham5861@gmail.com>
2023-08-15 14:25:49 +0200

    Tags class

--- src/main/java/com/vodafone/learningHub/repository/PostRepository.java
+++ src/main/java/com/vodafone/learningHub/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package com.vodafone.learningHub.repository;
 
 import com.vodafone.learningHub.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Integer> {
 }
