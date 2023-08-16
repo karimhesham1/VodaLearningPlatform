d490fce626a566ec129bb4f8f73c100fa96db98a
Fares Hesham <fares.h.moneim@gmail.com>
2023-08-16 20:53:08 +0300

    added test directory and added junit dependency

---
+++ src/test/java/com/vodafone/learningHub/LearningHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vodafone.learningHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
