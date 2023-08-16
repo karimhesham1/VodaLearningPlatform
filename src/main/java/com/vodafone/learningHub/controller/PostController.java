1f7a6e3f78eb2e43db9a02052260da3ceb17f996
Karim Hesham <karim.hesham5861@gmail.com>
2023-08-16 14:52:07 +0200

    PostServiceI

--- src/main/java/com/vodafone/learningHub/controller/PostController.java
+++ src/main/java/com/vodafone/learningHub/controller/PostController.java
@@ -1,16 +1,36 @@
 package com.vodafone.learningHub.controller;
 
+import com.vodafone.learningHub.model.Post;
+import com.vodafone.learningHub.openapi.api.PostApi;
+import com.vodafone.learningHub.openapi.model.PostRequest;
+import com.vodafone.learningHub.openapi.model.PostResponse;
 import com.vodafone.learningHub.service.PostServiceI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/post")
+
 @RequiredArgsConstructor
-public class PostController {
+public class PostController implements PostApi {
 
     private final PostServiceI postServiceI;
+
+
+    @Override
+    public ResponseEntity<PostResponse> uploadPost(PostRequest postRequest) {
+        try {
+            postServiceI.createPost();
+//            ResponseEntity.crea
+        } catch(IndexOutOfBoundsException | IllegalArgumentException ex) {
+
+        } catch (RuntimeException ex) {
+
+        }
+    }
+
 
 }
--- src/main/java/com/vodafone/learningHub/service/PostServiceI.java
+++ src/main/java/com/vodafone/learningHub/service/PostServiceI.java
@@ -1,9 +1,11 @@
 package com.vodafone.learningHub.service;
 
 import com.vodafone.learningHub.model.Post;
+import com.vodafone.learningHub.openapi.model.PostRequest;
+import com.vodafone.learningHub.openapi.model.PostResponse;
 
 public interface PostServiceI {
 
-    public Post createPost(Post post);
+    public PostResponse createPost(PostRequest postRequest);
     
 }
