c3ff52c6669bd00b391a2dbca5b414b59e924640
Karim Hesham <karim.hesham5861@gmail.com>
2023-08-17 11:18:20 +0200

    post feature done

--- src/main/java/com/vodafone/learningHub/controller/PostController.java
+++ src/main/java/com/vodafone/learningHub/controller/PostController.java
@@ -8,11 +8,14 @@ import com.vodafone.learningHub.service.PostServiceI;
 import lombok.RequiredArgsConstructor;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 
 @RequiredArgsConstructor
@@ -22,17 +25,17 @@ public class PostController implements PostApi  {
 
 
     @Override
-    public ResponseEntity<PostResponse> uploadPost(PostRequest postRequest) {
+    public ResponseEntity uploadPost(PostRequest postRequest) {
         try {
-            PostResponse post  = postServiceI.createPost(postRequest);
-//            ResponseEntity.crea
-        } catch(IndexOutOfBoundsException | IllegalArgumentException ex) {
+            PostResponse postResponse  = postServiceI.createPost(postRequest);
+            return ResponseEntity.status(HttpStatus.CREATED).body(postResponse);
 
-        } catch (RuntimeException ex) {
-
+        } catch(IllegalArgumentException ex) {
+            return ResponseEntity.badRequest().body(ex.getMessage());
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
 
-        return ResponseEntity.ok().build();
 
     }
 
