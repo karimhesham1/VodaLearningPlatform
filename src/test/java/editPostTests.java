b97311714d6aee5cf044bca53d4efc534e7b9efa
Karim Hesham <karim.hesham5861@gmail.com>
2023-08-21 15:10:54 +0200

    test scenarios for edit ft

---
+++ src/test/java/editPostTests.java
@@ -0,0 +1,10 @@
+public class editPostTests {
+    //scenarios
+
+//    Successful Post Edit (HTTP 202 Accepted)
+//    Attempt to Edit Nonexistent Post (HTTP 404 Not Found)
+//    Missing Required Fields (HTTP 400 Bad Request)
+//    Successful Post Edit with No Changes (HTTP 202 Accepted)
+
+
+}
